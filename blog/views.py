5e1ec537e2700123deac3a1719e59161e87c935a
rishabh <rishabh.bansal2200@gmail.com>
2020-05-01 23:27:37 +0530

    all blog in course

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import blog
 # Create your views here.
+def allblog(request):
+    rishabh=blog.objects.all()
+    return render(request,'blog/allblog.html',{'bansal':rishabh})
